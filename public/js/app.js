926e6b647558dd4e4dbfa4dfe92a8970f1ef4d56
Lesther <lesther.greenapple@gmail.com>
2021-09-07 18:06:45 +0800

    FIX: Master List

--- public/js/app.js
+++ public/js/app.js
@@ -14717,9 +14717,9 @@ try {
 
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'; // window.axios.defaults.baseURL = 'http://192.168.100.100/mtfru';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.baseURL = 'http://192.168.100.100/mtfru'; // window.axios.defaults.baseURL = 'http://localhost/pagadian-mtfru';
 
-window.axios.defaults.baseURL = 'http://localhost/pagadian-mtfru';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
