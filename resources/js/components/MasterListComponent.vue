ff0300323efd176a10203d9eda91938304a183a7
Lesther <lesther.greenapple@gmail.com>
2021-10-06 13:59:23 +0800

    Fix: Master list Date Registered Field

--- resources/js/components/MasterListComponent.vue
+++ resources/js/components/MasterListComponent.vue
@@ -184,6 +184,7 @@ export default {
     },
     methods: {
 
+
         formatPrice(value) {
             let val = (value/1).toFixed(2).replace(',', '.')
             return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
